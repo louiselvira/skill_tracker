96b224063186df5a2ba8f290364c8c1ae0992092
Louise <louise.stigell@gmail.com>
2015-09-15 23:45:11 +0200

    Switched to Bootstrap UI for progress bar and added a few functions.

--- app.js
+++ app.js
@@ -2,47 +2,41 @@
 // Louise Stigell
 
 //The user can: 
-//- Enter skills they'd like to start tracking
+//- Add and remove skills they'd like to start tracking
 //- Log hours to that skill
 //- See a progress bar that indicates how close they are to 10 000 hours ("mastery").
 //- Reach different "levels" throughout their skill tracking.
+//- Get a weekly overview of their skill progress
 
-var skillTracker = angular.module("skillTracker", ['ngProgress']); //"Hello, I'm an Angular app."
+var skillTracker = angular.module("skillTracker", ['ui.bootstrap']); //"Hello, I'm an Angular app."
 
-skillTracker.controller('mainController', function ($scope, ngProgressFactory) {
+skillTracker.controller('mainController', function ($scope) {
 	var title = "Welcome to SkillTracker";
 	$scope.title = title;
 	
-	var mySkills = [];
+	//Container array for skills:
+	var mySkills = []; 
 	$scope.mySkills = mySkills;
 	
+	//Constructor for skill objects:
 	function Skill(skillName) {
     	this.name = skillName;
     	this.hoursLogged = 0;
     	this.level = 1;
-		this.progressbar = ngProgressFactory.createInstance();
-		var logHour = function () {
-			this.hoursLogged += 1;
-			this.progressbar.set(this.progressbar.status += 5);	
+		this.logHour = function (hours) { //Function to log hours to skill
+			this.hoursLogged = this.hoursLogged + hours;
 		};
 	}
 
-	
+	//Container for capturing name of new skill to create a new skill object:
 	var skillName = "";
 	$scope.skillName = skillName;
 	
+	//Saves the new skill to skill container:
 	$scope.save = function (skillName) {
 		var skill = new Skill(skillName);
-		skill.progressbar.set(3);
 		mySkills.push(skill);
 	};
 	
-	/*$scope.logHours = function (skill) {
-		skill.hoursLogged += 1;
-		skill.progressbar.set(skill.progressbar.status += 5);
-	};*/
-	
-	//Clear input field after save.
-	//Show progress bar in skill list.
 	
 });
